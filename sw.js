9d2e4e30c135840f1de2d99cab0234b8d33214e9
zxuan-c <marshall1841@gmail.com>
2026-06-11 15:15:51 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,66 @@
+const CACHE_NAME = "badminton-play-v1";
+const appUrl = (path = "") => new URL(path, self.registration.scope).toString();
+const APP_SHELL = [
+  appUrl(),
+  appUrl("index.html"),
+  appUrl("manifest.webmanifest"),
+  appUrl("icons/app-icon.svg"),
+  appUrl("icons/app-icon-180.png"),
+  appUrl("icons/app-icon-192.png"),
+  appUrl("icons/app-icon-512.png"),
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting()),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key)),
+        ),
+      )
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(appUrl(), copy));
+          return response;
+        })
+        .catch(() => caches.match(appUrl())),
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(
+      (cached) =>
+        cached ??
+        fetch(event.request).then((response) => {
+          if (response.ok && new URL(event.request.url).origin === self.location.origin) {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          }
+          return response;
+        }),
+    ),
+  );
+});
