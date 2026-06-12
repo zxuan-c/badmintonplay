2a4ae0e257cb563b03fe79e876056c21ae176d80
zxuan-c <marshall1841@gmail.com>
2026-06-12 10:22:40 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "badminton-play-v4";
+const CACHE_NAME = "badminton-play-v5";
 const appUrl = (path = "") => new URL(path, self.registration.scope).toString();
 const APP_SHELL = [
   appUrl(),
